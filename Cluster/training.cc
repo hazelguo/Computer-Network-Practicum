87ac41ea424b37dae592f1cb450848f807f92bd7
郭秭含 <gzh@gzh-rmbp.local>
2016-04-16 19:05:46 +0800

    Debug training.cc and now it works well with fabricated inputs.

--- Cluster/training.cc
+++ Cluster/training.cc
@@ -4,12 +4,13 @@
 #include <cstdlib>
 #include <unordered_map>
 #include <unordered_set>
+#include <iostream>
 
 using namespace std;
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
-const int knpass = 1;
+const int knpass = 10;
 
 class Cluster {
 	public:
@@ -54,7 +55,9 @@ Cluster::Cluster() {
 	nschools = 0;
 	cluster_result.clear();
 	distance = NULL;
+	Kvalue.clear();
 	schools.clear();
+	choice.clear();
 }
 
 Cluster::~Cluster() {
@@ -66,6 +69,7 @@ Cluster::~Cluster() {
 }
 
 void Cluster::SetInputs() {
+	// Read from input.
 	scanf("%d%d", &nelements, &nschools);
 	
 	distance = (double**)calloc(nelements, sizeof(double*));
@@ -74,6 +78,7 @@ void Cluster::SetInputs() {
 		for (int j = 0; j < i; ++j) {
 			scanf("%lf", &distance[i][j]);
 		}
+		
 	}	
 	
 	for (int i = 0; i < nschools; ++i) {
@@ -85,12 +90,21 @@ void Cluster::SetInputs() {
 			students.push_back(k);
 		}
 		schools.push_back(students);
+	}
+	
+	// Initialize other member variables.
+	unordered_set<int> tmp_set;
+	tmp_set.clear();
+	for (int i = 0; i < nelements; ++i) {
+		choice.push_back(tmp_set);
+	}
+	for (int i = 0; i < nschools; ++i) {
 		Kvalue.push_back(0);
-	}	
+	}
 }
 
 int Cluster::CalcCostFunction(int schoolid, int *clusterid) {
-	int Ret = 0;
+	int Ret = 1;
 	for (int i = 0; i < schools[schoolid].size(); ++i) {
 		for (int j = i+1; j < schools[schoolid].size(); ++j) {
 			Ret += (clusterid[i] != clusterid[j]);
@@ -109,12 +123,13 @@ void Cluster::CalcClusterResult() {
 		clusterid = (int*)calloc(nelements, sizeof(int));
 		kmedoids(i, nelements, distance, knpass, clusterid, &error, &ifound);
 		cluster_result.push_back(clusterid);
-		
+	
+		error = max(1, log(max(error, 1)));
 		for (int j = 0; j < nschools; ++j) {
 			double cost = CalcCostFunction(j, clusterid) * error;
 			if (cost < min_cost[j]) {
 				min_cost[j] = cost;
-				Kvalue[j] = i;
+				Kvalue[j] = i-1;
 			}
 		}
 	}
@@ -124,7 +139,7 @@ void Cluster::CalcSelectedStudents(int schoolid) {
 	// Sum the amount of accepted students in each cluster.
 	unordered_map<int, int> count_cluster;
 	for (int i = 0; i < schools[schoolid].size(); ++i) {
-		count_cluster[cluster_result[Kvalue[schoolid]][i]] ++;
+		count_cluster[cluster_result[Kvalue[schoolid]][schools[schoolid][i]]] ++;
 	}
 	
 	// Find the maximum of accepted students in one cluster.
@@ -134,14 +149,14 @@ void Cluster::CalcSelectedStudents(int schoolid) {
 	}
 	
 	// Calc the cluster ids which is acceptible for school 'schoolid'.
-	max_cluster >>= 1;
+	max_cluster = (int) (max_cluster * 0.5 + 0.5);
 	unordered_set<int> accepted_cluster;
 	for (unordered_map<int, int>::iterator iter = count_cluster.begin(); iter != count_cluster.end(); ++iter) {
 		if (iter->second >= max_cluster) {
 			accepted_cluster.insert(iter->first);
 		}
 	}
-
+	
 	// Update choice.
 	for (int i = 0; i < nelements; ++i) {
 		if (accepted_cluster.find(cluster_result[Kvalue[schoolid]][i]) != accepted_cluster.end()) {
@@ -152,6 +167,7 @@ void Cluster::CalcSelectedStudents(int schoolid) {
 
 void Cluster::Output() {
 	for (int i = 0; i < nelements; ++i) {
+		printf("%lu ", choice[i].size());
 		for (unordered_set<int>::iterator iter = choice[i].begin(); iter != choice[i].end(); ++iter) {
 			printf("%d ", *iter);
 		}
